7a3683d64edceed8a479ae67c51d8012d7a40d86
Sebastian Perdomo Rengifo <Sapr262004@gmail.com>
2025-03-11 21:09:16 -0500

    update(Front)

--- Login/script.js
+++ Login/script.js
@@ -1,102 +1,69 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const registerLink = document.querySelector(".registrarse-link");
-    const loginLink = document.querySelector(".login-link");
-    const recuperarLink = document.querySelector(".recuperar-link");
-    const loginForm = document.querySelector(".login-form");
-    const registerForm = document.querySelector(".register-form");
-    const recuperarForm = document.querySelector(".rest-contra");
+document.addEventListener('DOMContentLoaded', function() {
+    const formulario = document.querySelector('.formulario');
+    const loginForm = document.querySelector('.login-form');
+    const registerForm = document.querySelector('.register-form');
+    const restContraForm = document.querySelector('.rest-contra');
+    const loginLink = document.querySelector('.login-link');
+    const registrarseLink = document.querySelector('.registrarse-link');
+    const recuperarLink = document.querySelector('.recuperar-link');
+    const nuevoUsuario = document.querySelector('.nuevousuario');
+    const restablecer = document.querySelector('.restablecer');
 
-    // Función para cambiar al formulario de registro
-    registerLink.addEventListener("click", function(event) {
-        event.preventDefault();
-        if (loginForm.style.display !== "none") {
-            loginForm.style.transition = "opacity 0.8s ease-in-out";
-            loginForm.style.opacity = 0;
-            setTimeout(function() {
-                loginForm.style.display = "none";
-                registerForm.style.transition = "opacity 0.8s ease-in-out";
-                registerForm.style.opacity = 1;
-                registerForm.style.display = "block";
-            }, 800);
-        }
+    // Función para ajustar el tamaño del formulario
+    function ajustarTamañoFormulario(formularioActivo) {
+        const alturaFormulario = formularioActivo.offsetHeight;
+        const anchoFormulario = formularioActivo.offsetWidth;
+        formulario.style.height = `${alturaFormulario}px`;
+        formulario.style.maxWidth = `${anchoFormulario}px`;
+    }
+
+    // Evento para el formulario de registro
+    registrarseLink.addEventListener('click', function(e) {
+        e.preventDefault();
+        setTimeout(() => {
+            formulario.style.transform = 'translateX(100%)'; // Mover a la derecha
+            nuevoUsuario.classList.add('active-registro');
+            restablecer.classList.remove('active-restablecer');
+            loginForm.classList.add('hidden');
+            ajustarTamañoFormulario(registerForm); // Ajustar tamaño para el formulario de registro
+        }, 10); // Pequeño retraso para permitir el renderizado
     });
 
-    // Función para cambiar al formulario de inicio de sesión
-    loginLink.addEventListener("click", function(event) {
-        event.preventDefault();
-        if (registerForm.style.display !== "none") {
-            registerForm.style.transition = "opacity 0.8s ease-in-out";
-            registerForm.style.opacity = 0;
-            setTimeout(function() {
-                registerForm.style.display = "none";
-                loginForm.style.transition = "opacity 0.8s ease-in-out";
-                loginForm.style.opacity = 1;
-                loginForm.style.display = "block";
-            }, 800);
-        }
+    // Evento para el formulario de restablecer contraseña
+    recuperarLink.addEventListener('click', function(e) {
+        e.preventDefault();
+        setTimeout(() => {
+            formulario.style.transform = 'translateX(-100%)'; // Mover a la izquierda
+            restablecer.classList.add('active-restablecer');
+            nuevoUsuario.classList.remove('active-registro');
+            loginForm.classList.add('hidden');
+            ajustarTamañoFormulario(restContraForm); // Ajustar tamaño para el formulario de restablecimiento
+        }, 10); // Pequeño retraso para permitir el renderizado
     });
 
-    // Función para cambiar al formulario de restablecimiento de contraseña
-    recuperarLink.addEventListener("click", function(event) {
-        event.preventDefault();
-        if (loginForm.style.display !== "none") {
-            loginForm.style.transition = "opacity 0.8s ease-in-out";
-            loginForm.style.opacity = 0;
-            setTimeout(function() {
-                loginForm.style.display = "none";
-                recuperarForm.style.transition = "opacity 0.8s ease-in-out";
-                recuperarForm.style.opacity = 1;
-                recuperarForm.style.display = "block";
-            }, 800);
-        }
+    // Evento para volver al inicio de sesión desde el registro
+    const volverInicioDesdeRegistro = document.querySelector('.register-form .login-link');
+    volverInicioDesdeRegistro.addEventListener('click', function(e) {
+        e.preventDefault();
+        setTimeout(() => {
+            formulario.style.transform = 'translateX(0)'; // Volver al centro
+            nuevoUsuario.classList.remove('active-registro');
+            restablecer.classList.remove('active-restablecer');
+            loginForm.classList.remove('hidden');
+            ajustarTamañoFormulario(loginForm); // Ajustar tamaño para el formulario de inicio de sesión
+        }, 10); // Pequeño retraso para permitir el renderizado
     });
 
-    // Función para cambiar al formulario de inicio de sesión desde el formulario de restablecimiento de contraseña
-    const backToLoginLink = document.querySelector(".restablecer .login-link");
-    backToLoginLink.addEventListener("click", function(event) {
-        event.preventDefault();
-        if (recuperarForm.style.display !== "none") {
-            recuperarForm.style.transition = "opacity 0.8s ease-in-out";
-            recuperarForm.style.opacity = 0;
-            setTimeout(function() {
-                recuperarForm.style.display = "none";
-                loginForm.style.transition = "opacity 0.8s ease-in-out";
-                loginForm.style.opacity = 1;
-                loginForm.style.display = "block";
-            }, 800);
-        }
-    });
-});
-
-
-document.getElementById('loginForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-
-    const formData = new FormData(event.target);
-    const email = formData.get('email');
-    const contrasena = formData.get('contrasena');
-
-    fetch('/login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ email, contrasena })
-    })
-    .then(response => {
-        if (!response.ok) {
-            return response.text().then(errorMessage => {
-                alert(errorMessage);
-            });
-        } else {
-            return response.text().then(successMessage => {
-                alert(successMessage);
-                window.location.href = '/perfil';
-            });
-        }
-    })
-    .catch(error => {
-        console.error('Error al iniciar sesión:', error);
-        alert('Se produjo un error al iniciar sesión');
+    // Evento para volver al inicio de sesión desde restablecer
+    const volverInicioDesdeRestablecer = document.querySelector('.rest-contra .login-link');
+    volverInicioDesdeRestablecer.addEventListener('click', function(e) {
+        e.preventDefault();
+        setTimeout(() => {
+            formulario.style.transform = 'translateX(0)'; // Volver al centro
+            restablecer.classList.remove('active-restablecer');
+            nuevoUsuario.classList.remove('active-registro');
+            loginForm.classList.remove('hidden');
+            ajustarTamañoFormulario(loginForm); // Ajustar tamaño para el formulario de inicio de sesión
+        }, 10); // Pequeño retraso para permitir el renderizado
     });
 });
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # PaperTrail
- Tienda Online de libros :D
